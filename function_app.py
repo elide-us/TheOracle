075535a2a2fb8635821ee6835749268a70dd5e13
Aaron Stackpole <aaron@elideus.net>
2024-10-10 09:53:05 -0700

    Updated app registration requirement for Function App

--- function_app.py
+++ function_app.py
@@ -49,7 +49,8 @@ msal_app = ConfidentialClientApplication(
     client_credential=CLIENT_SECRET
 )
 
-@app.route(route="HttpExample")
+@app.function_name(name="HttpExample")
+@app.route(route="HttpExample", auth_level=func.AuthLevel.FUNCTION)
 def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
