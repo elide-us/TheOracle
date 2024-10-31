f51ac39f5d2cda2925b2cff38fe3e0bfc3bf15bf
Elideus <aaron@elideus.net>
2024-10-31 15:47:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
 
-# Create the Flask application instance
 app = Flask(__name__)
 
-# Define a route for the index page
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+@app.route('/cpl')
+def control_panel():
+    return render_template('control_panel.html')
 
-# Run the application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
