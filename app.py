2955aae3893a5399d20001908658aae029f4aed8
Aaron Stackpole <aaron@elideus.net>
2024-10-31 11:21:41 -0700

    Border styling

--- app.py
+++ app.py
@@ -11,3 +11,26 @@ def index():
 # Run the application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+#   <meta charset="UTF-8">
+#   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#   <title>Hello World</title>
+#   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+# </head>
+# <body>
+#   <div class="container">
+#     <div class="row">
+#         <div class="col-md-12 text-center">
+#           <div class="card mt-5" style="display: inline-block;">
+#             <div class="card-body">
+#               <h1 class="display-4 text-primary">Hello World</h1>
+#             </div>
+#           </div>
+#         </div>
+#       </div>
+#     </div>
+# </body>
+# </html>
