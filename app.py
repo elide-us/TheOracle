33a372c4ef6f0a36b86ed46af54d00a7b92da2e9
Aaron Stackpole <aaron@elideus.net>
2024-10-31 10:35:41 -0700

    Hello World

--- app.py
+++ app.py
@@ -1,69 +1,13 @@
-import identity.web
-import requests
-from flask import Flask, redirect, render_template, request, session, url_for
-from flask_session import Session
+from flask import Flask, render_template
 
-import app_config
-
+# Create the Flask application instance
 app = Flask(__name__)
-app.config.from_object(app_config)
-assert app.config["REDIRECT_PATH"] != "/", "REDIRECT_PATH must not be /"
-Session(app)
 
-#
-# middleware stuff?
-#
-
-app.jinja_env.globals.update(Auth=identity.web.Auth)  # Useful in template for B2C
-auth = identity.web.Auth(
-    session=session,
-    authority=app.config["AUTHORITY"],
-    client_id=app.config["CLIENT_ID"],
-    client_credential=app.config["CLIENT_SECRET"],
-)
-
-@app.route("/login")
-def login():
-    return render_template("login.html", version=__version__, **auth.log_in(
-        scopes=app_config.SCOPE, # Have user consent to scopes during log-in
-        redirect_uri=url_for("auth_response", _external=True), # Optional. If present, this absolute URL must match your app's redirect_uri registered in Microsoft Entra admin center
-        prompt="select_account",  # Optional.
-        ))
-
-@app.route(app_config.REDIRECT_PATH)
-def auth_response():
-    result = auth.complete_log_in(request.args)
-    if "error" in result:
-        return render_template("auth_error.html", result=result)
-    return redirect(url_for("index"))
-
-@app.route("/logout")
-def logout():
-    return redirect(auth.log_out(url_for("index", _external=True)))
-
-@app.route("/")
+# Define a route for the index page
+@app.route('/')
 def index():
-    if not (app.config["CLIENT_ID"] and app.config["CLIENT_SECRET"]):
-        # This check is not strictly necessary.
-        # You can remove this check from your production code.
-        return render_template('config_error.html')
-    if not auth.get_user():
-        return redirect(url_for("login"))
-    return render_template('index.html', user=auth.get_user(), version=__version__)
+    return render_template('index.html')
 
-@app.route("/call_downstream_api")
-def call_downstream_api():
-    token = auth.get_token_for_user(app_config.SCOPE)
-    if "error" in token:
-        return redirect(url_for("login"))
-    # Use access token to call downstream api
-    api_result = requests.get(
-        app_config.ENDPOINT,
-        headers={'Authorization': 'Bearer ' + token['access_token']},
-        timeout=30,
-    ).json()
-    return render_template('display.html', result=api_result)
-
-if __name__ == "__main__":
-    app.run()
-
+# Run the application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
