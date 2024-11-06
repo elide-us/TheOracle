4fe604937a1a4092319f7f9eaf3c61e7691a392f
Aaron Stackpole <aaron@elideus.net>
2024-11-05 20:15:36 -0800

    Added React Route

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory, render_template
 import os
 import msal
 
@@ -6,11 +6,15 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 TENANT_ID = os.getenv('TENANT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/react')
+def react():
+    return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/login')
 def login():
