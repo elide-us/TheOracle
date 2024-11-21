63d165459fabb540c0b84a62d27e1d5f8a0fbec9
Elideus <aaron@elideus.net>
2024-11-21 15:48:11 -0800

    Update bot.py

--- backend/bot.py
+++ backend/bot.py
@@ -3,4 +3,16 @@ import discord
 
 DISCORD_SECRET = os.getenv('DISCORD_SECRET')
 
-# do bot shit
+intents = discord.Intents.default()
+intents.messages = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+  print("Bot online.")
+
+@bot.command(name="ai")
+async def ping(ctx):
+  await ctx.send("Online.")
+
+bot.run(DISCORD_SECRET)
