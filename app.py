479717ba268630276a39d3b92f93d60cd8eb5fc8
Elideus <aaron@elideus.net>
2024-11-22 17:29:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def index():
   return "Hello World!"
 
-@app.route('/discord')
+@app.route('/discord', methods=['GET', 'POST'])
 def discord():
-  return "Hello World!"
+  if request.method == 'POST':
+    data = request.get_json()
+    if data and 'content' in data:
+      return jsonify({"content":data['content']})
+    else:
+      return jsonify({"error":"Missing 'content' in payload"}), 400
+  else:
+    return "Hello World!"
+
+# Remember, `app` will be called directly from gunicorn as `app:app`
