5cf634854d501e01ee29132474c6a9d721aabba2
Elideus <aaron@elideus.net>
2024-12-13 20:33:58 -0800

    Chat command handler additional debugging

--- backend/commands/text_commands.py
+++ backend/commands/text_commands.py
@@ -1,5 +1,6 @@
 from utils.messaging import send_to_discord
 from services.local_json import load_json
+import discord
 
 async def generate_text(prompt: str, client, channel) -> None:
   await channel.send("Sending text prompt to OpenAI.")
@@ -22,6 +23,7 @@ async def handle_text_generate(args: str, channel, client):
   return await generate_text(prompt, client, channel)
 
 async def handle_chat(ctx, command_str):
+  await ctx.send("Parsing chat command.")
   app = ctx.bot.app
   bot = ctx.bot
   channel = ctx.channel
@@ -31,31 +33,40 @@ async def handle_chat(ctx, command_str):
   key = split[0]
   prompt = split[1:]
 
+  await channel.send(f"Loading assistant data from data_assistants.json.")
   json = await load_json("data_assistants.json")
   if not json:
     await channel.send("Error loading assistant data.")
     return
 
+  await channel.send(f"Loading assistant data for {key}.")
   assistant = json[key]
   if not assistant:
     await channel.send(f"Error loading assistant: {key} not found.")
     return
 
   assistant.prompt = " ".join(prompt)
+  await channel.send(f"Assistant prompt: {assistant.prompt}")
 
-  # Open the data_templates.json file and load it
-  template_data = await load_json("data_templates.json")
-  if not template_data:
-    await channel.send("Error loading template data.")
-    return
+  # await channel.send("Loading template data from data_templates.json.")
+  # template_data = await load_json("data_templates.json")
+  # if not template_data:
+  #   await channel.send("Error loading template data.")
+  #   return
+  # await channel.send(f"Loading template data for chatresponse.")
+  # template = template_data["chatresponse"]
+  # if not template:
+  #   await channel.send("Error loading template: chatresponse not found.")
+  #   return
+  # prompt = template.format(**assistant)
 
-  template = template_data["chatresponse"]
-  if not template:
-    await channel.send("Error loading template: chatresponse not found.")
-    return
-
-  prompt = template.format(**assistant)
-
-  completion = await client.chat.completions.create(prompt)
+  completion = await client.chat.completions.create(
+    model=f"{assistant.model}",
+    max_completion_tokens=f"{assistant.max_tokens}",
+    messages=[
+      {"role":"system","content":f"{assistant.role}"},
+      {"role":"user","content":f"{assistant.prompt}"}
+    ]
+  )
   response_text = completion.choices[0].message.content
   await send_to_discord(bot.sys_channel, response_text)
