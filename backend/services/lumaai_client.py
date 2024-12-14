61a4950e2079679cedc45232b8c202967827f546
Elideus <aaron@elideus.net>
2024-12-13 19:18:27 -0800

    LumaAI get token is not async

--- backend/services/lumaai_client.py
+++ backend/services/lumaai_client.py
@@ -2,5 +2,5 @@ from lumaai import AsyncLumaAI, LumaAI
 from config import get_lumaai_token
 
 async def init_lumaai_client():
-  token = await get_lumaai_token()
-  return LumaAI(auth_token=token)
+  token = get_lumaai_token()
+  return AsyncLumaAI(auth_token=token)
