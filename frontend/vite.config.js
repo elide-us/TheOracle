7fcd1cef5e4aeb47596ae9ba3d3579d35b3f072f
Elideus <aaron@elideus.net>
2024-12-25 09:42:50 -0800

    base config for prod

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
     return {
         plugins: [react()],
-        base: process.env.VITE_BASE_URL || '/',
+        base: '/static/',
         build: {
           outDir: 'dist',
           assetsDir: 'assets',
