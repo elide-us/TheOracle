1dee592dc77435ce31169b9af9b2cc75e7b91ddb
Elideus <aaron@elideus.net>
2024-12-25 11:35:45 -0800

    Additionall scaffolding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,28 +4,36 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { Container, Box } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TheOracleTheme from './config/theoracletheme';
-import Home from './components/Home';
 import Sidebar from './components/Sidebar';
+import Home from './components/Home';
+import FileManager from './components/FileManager';
+import Gallery from './components/Gallery';
+import TheOracleGPT from './components/TheOracleGPT';
+import Prism from './components/Prism';
 
 
 function App() {
-    const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-    return (
-        <ThemeProvider theme={ TheOracleTheme }>
-            <CssBaseline />
-            <Router>
-                <Container sx={{ width: '100%', display: 'block' }}>
-                    <Sidebar open={open} setOpen={setOpen} />
-                    <Box sx={{ position: 'relative', left: '30px' }}>
-                        <Routes>
-                            <Route path='/' element={<Home />} />
-                        </Routes>
-                    </Box>
-                </Container>
-            </Router>
-        </ThemeProvider>
-    );
+	return (
+		<ThemeProvider theme={ TheOracleTheme }>
+			<CssBaseline />
+			<Router>
+				<Container sx={{ width: '100%', display: 'block' }}>
+					<Sidebar open={open} setOpen={setOpen} />
+					<Box sx={{ position: 'relative', left: '30px' }}>
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/file-manager' element={<FileManager />} />
+							<Route path='gallery' element={ <Gallery /> } />
+							<Route path='the-oracle-gpt' element={ <TheOracleGPT /> } />
+							<Route path='prism' element={ <Prism /> } />
+						</Routes>
+					</Box>
+				</Container>
+			</Router>
+		</ThemeProvider>
+	);
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/TheOracleGPT.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TheOracleGPT() {
+    return (
+        <p>TheOracleGPT</p>
+    )
+}
+
+export default TheOracleGPT;
