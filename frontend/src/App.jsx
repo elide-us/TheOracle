dec7a0416264b9e7ce47f6157d936f0d1af5120d
Elideus <aaron@elideus.net>
2024-12-25 07:57:21 -0800

    New frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Container, Box } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TheOracleTheme from './config/theoracletheme';
+import Home from './components/Home';
+import Sidebar from './components/Sidebar';
+
+
+function App() {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <ThemeProvider theme={ TheOracleTheme }>
+            <CssBaseline />
+            <Router>
+                <Container sx={{ width: '100%', display: 'block' }}>
+                    <Sidebar open={open} setOpen={setOpen} />
+                    <Box sx={{ position: 'relative', left: '30px' }}>
+                        <Routes>
+                            <Route path='/' element={<Home />} />
+                        </Routes>
+                    </Box>
+                </Container>
+            </Router>
+        </ThemeProvider>
+    );
+}
+
+export default App;
+
+
+
---
+++ frontend/src/config/links.jsx
@@ -0,0 +1,10 @@
+const Links = [
+    { title: 'Discord', url: 'https://discord.gg/xXUZFTuzSw' },
+    { title: 'GitHub', url: 'https://github.com/elide-us' },
+    { title: 'TikTok', url: 'https://www.tiktok.com/@elide.us' },
+    { title: 'BlueSky', url: 'https://bsky.app/profile/elideusgroup.com' },
+    { title: 'Instagram', url: 'https://www.instagram.com/elide.us/profilecard/?igsh=MWR0dGhxdzI1b2E0Mg==' },
+    { title: 'Suno', url: 'https://suno.com/@elideus' }
+]
+
+export default Links;
