8d7527e1cfe3f5ef55d8ceddb36699ae81a5e814
Elideus <aaron@elideus.net>
2024-12-26 11:07:37 -0800

    Ignore catch

--- frontend/src/components/FileManager.jsx
+++ frontend/src/components/FileManager.jsx
@@ -3,35 +3,33 @@ import axios from 'axios';
 import LinkIcon from '@mui/icons-material/Link';
 
 function FileManager() {
-  const [files, setFiles] = useState([]);
-  const [loading, setLoading] = useState(true);
+const [files, setFiles] = useState([]);
+const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    axios.get('/api/files').then(response => {
-      if (response.data && Array.isArray(response.data.files)) {
-        setFiles(response.data.files);
-      } else {
-        setFiles([]);
-      }
-      setLoading(false);
-    }).catch(error => {
-      setLoading(false);
-    });
-  }, []);
+useEffect(() => {
+	axios.get('/api/files').then(response => {
+		if (response.data && Array.isArray(response.data.files)) {
+			setFiles(response.data.files);
+		} else {
+			setFiles([]);
+		}
+		setLoading(false);
+	});
+}, []);
 
-  if (loading) return <p>Loading files...</p>;
+if (loading) return <p>Loading files...</p>;
 
-  return (
-    <ul>
-      {files.map(file => (
-        <li key={file.name}>
-        	{file.name} <a href={file.url} target="_blank" rel="noopener noreferrer">
-        	<LinkIcon />
-        </a>
-      </li>
-      ))}
-    </ul>
-  );
+return (
+	<ul>
+		{files.map(file => (
+			<li key={file.name}>
+				{file.name} <a href={file.url} target="_blank" rel="noopener noreferrer">
+					<LinkIcon />
+				</a>
+			</li>
+		))}
+	</ul>
+);
 }
 
 export default FileManager;
