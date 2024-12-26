e974e6fd9492f5a08d75750c4d1cdfbfa93baae7
Elideus <aaron@elideus.net>
2024-12-26 11:22:52 -0800

    MUI Upudate

--- frontend/src/components/FileManager.jsx
+++ frontend/src/components/FileManager.jsx
@@ -1,35 +1,33 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import LinkIcon from '@mui/icons-material/Link';
+import { Link as LinkIcon, List, ListItem, Typography } from '@mui/icons-material';
 
 function FileManager() {
-const [files, setFiles] = useState([]);
-const [loading, setLoading] = useState(true);
+	const [files, setFiles] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-	axios.get('/api/files').then(response => {
-		if (response.data && Array.isArray(response.data.files)) {
-			setFiles(response.data.files);
-		} else {
-			setFiles([]);
-		}
-		setLoading(false);
-	});
-}, []);
+	useEffect(() => {
+		axios.get('/api/files').then(response => {
+			if (response.data && Array.isArray(response.data.files)) {
+				setFiles(response.data.files);
+			} else {
+				setFiles([]);
+			}
+			setLoading(false);
+		});
+	}, []);
 
-if (loading) return <p>Loading files...</p>;
+	if (loading) return <Typography variant='p'>Loading files...</Typography>;
 
-return (
-	<ul>
-		{files.map(file => (
-			<li key={file.name}>
-				{file.name} <a href={file.url} target="_blank" rel="noopener noreferrer">
-					<LinkIcon />
-				</a>
-			</li>
-		))}
-	</ul>
-);
+	return (
+		<List>
+			{files.map(file => (
+				<ListItem key={file.name}>
+					<Typography variant='p'>{file.name}</Typography> <a href={file.url} target="_blank" rel="noopener noreferrer"><LinkIcon /></a>
+				</ListItem>
+			))}
+		</List>
+	);
 }
 
 export default FileManager;
