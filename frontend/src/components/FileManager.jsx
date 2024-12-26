d3db15f5b17a083b3492b75ff6726fcf013e73d6
Elideus <aaron@elideus.net>
2024-12-26 09:49:38 -0800

    FileManager Test

--- frontend/src/components/FileManager.jsx
+++ frontend/src/components/FileManager.jsx
@@ -4,34 +4,35 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 function FileManager() {
-    const [files, setFiles] = useState([]);
-    const [loading, setLoading] = useState(true);
+    return <Typography>File Manager</Typography>
+    // const [files, setFiles] = useState([]);
+    // const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        axios.get('/api/files').then(response => {
-            if (response.data && Array.isArray(response.data.files)) {
-                setFiles(response.data.files);
-            } else {
-                setFiles([]);
-            }
-            setLoading(false);
-        }).catch(e => {
-            setLoading(false);
-        });
-    }, []);
+    // useEffect(() => {
+    //     axios.get('/api/files').then(response => {
+    //         if (response.data && Array.isArray(response.data.files)) {
+    //             setFiles(response.data.files);
+    //         } else {
+    //             setFiles([]);
+    //         }
+    //         setLoading(false);
+    //     }).catch(e => {
+    //         setLoading(false);
+    //     });
+    // }, []);
 
-    if (loading) return (<Typography>Loading files...</Typography>);
+    // if (loading) return (<Typography>Loading files...</Typography>);
 
-    return (
-        <List>
-            {files.map(file => (
-                <ListItem>
-                    <ListItemText><Typography>{file.name}</Typography></ListItemText>
-                    <Link href={file.url}><LinkIcon /></Link>
-                </ListItem>
-            ))}
-        </List>
-    );
+    // return (
+    //     <List>
+    //         {files.map(file => (
+    //             <ListItem>
+    //                 <ListItemText><Typography>{file.name}</Typography></ListItemText>
+    //                 <Link href={file.url}><LinkIcon /></Link>
+    //             </ListItem>
+    //         ))}
+    //     </List>
+    // );
 }
 
 export default FileManager;
