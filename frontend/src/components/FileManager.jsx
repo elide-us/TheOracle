7bfcb854a0521414b950edb3b684098b36bf70e6
Elideus <aaron@elideus.net>
2024-12-26 11:25:57 -0800

    Updated import

--- frontend/src/components/FileManager.jsx
+++ frontend/src/components/FileManager.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link as LinkIcon, List, ListItem, Typography } from '@mui/icons-material';
+import { Link as LinkIcon } from '@mui/icons-material';
+import { List, ListItem, Typography } from '@mui/material';
 
 function FileManager() {
 	const [files, setFiles] = useState([]);
