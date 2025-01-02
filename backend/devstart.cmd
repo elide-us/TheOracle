f7ef63fc15343b71b1ecc52c1582b64a15fec0bc
Elideus <aaron@elideus.net>
2025-01-02 11:27:38 -0800

    Updated local build script

--- backend/devstart.cmd
+++ backend/devstart.cmd
@@ -1,5 +1,10 @@
 @echo on
 cd ../frontend
+call npm run lint
+IF ERRORLEVEL 1 (
+    echo "npm run build failed. Exiting."
+    exit /b 1
+)
 call npm run build
 IF ERRORLEVEL 1 (
     echo "npm run build failed. Exiting."
@@ -7,9 +12,5 @@ IF ERRORLEVEL 1 (
 )
 cd ..
 robocopy frontend\dist backend\static /MIR
-IF ERRORLEVEL 1 (
-    echo "robocopy failed. Exiting."
-    exit /b 1
-)
 cd backend
 python -m uvicorn main:app --reload
