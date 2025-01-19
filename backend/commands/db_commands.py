27aab7b46619f0b25421f9204f30dffd0dbd9348
Elideus <aaron@elideus.net>
2025-01-19 15:17:12 -0800

    Debug message cleanup

--- backend/commands/db_commands.py
+++ backend/commands/db_commands.py
@@ -50,10 +50,6 @@ async def get_layer_template(pool, layer_id):
     return result
 
 async def get_user_from_database(app, pool, microsoft_id):
-  bot = app.state.discord_bot
-  channel = bot.get_channel(bot.sys_channel)
-  await channel.send("get_user_from_database()")
-
   async with pool.acquire() as conn:
     query = """
       SELECT guid, microsoft_id, email, username FROM users WHERE microsoft_id = $1
@@ -61,26 +57,16 @@ async def get_user_from_database(app, pool, microsoft_id):
     result = await conn.fetchrow(query, microsoft_id)
     if isinstance(result, str):
       result = json.loads(result)
-    await channel.send(f"Result from select: {result}")
     return result
   
 async def make_new_user_for_database(app, pool, microsoft_id, email, username):
-  bot = app.state.discord_bot
-  channel = bot.get_channel(bot.sys_channel)
-  await channel.send("make_new_user_for_database()")
-
   new_guid = str(uuid.uuid4())
-
-  await channel.send(f"No user found for Microsoft ID: {microsoft_id}")
-  await channel.send(f"Creating new user with GUID: {new_guid}")
-  
   async with pool.acquire() as conn:
     query = """
         INSERT INTO users (guid, microsoft_id, email, username)
         VALUES ($1, $2, $3, $4);
     """
     await conn.execute(query, new_guid, microsoft_id, email, username)
-    await channel.send("Executed INSERT query")
 
     result = await get_user_from_database(app, pool, microsoft_id)
     return result
