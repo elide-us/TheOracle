4ce2849cecfb7df1c54b21b7574533f187bc9997
Elideus <aaron@elideus.net>
2025-01-27 14:52:25 -0800

    Credits number formatting by locale

--- frontend/src/components/shared/Login.jsx
+++ frontend/src/components/shared/Login.jsx
@@ -76,7 +76,7 @@ function Login({open}) {
 						{userData.username}
 					</Typography>
 					<Typography component="span" variant="body2" sx={{ display: "block", fontSize: "0.9em", color: "gray" }}>
-					  {userData.credits}
+						{new Intl.NumberFormat(navigator.language).format(userData.credits)}
 					</Typography>
 				  </Box>) : "Login"} sx={{ marginLeft: "8px" }} />
 			)}
