f50ecd420cba362bfb726bf60ff082b7e53467c9
Elideus <aaron@elideus.net>
2025-02-09 08:39:20 -0800

    safe filenames

--- backend/commands/storage.py
+++ backend/commands/storage.py
@@ -1,6 +1,7 @@
 from utils.helpers import StateHelper
 
-async def write_buffer_to_blob(buffer, state: StateHelper, filename):
+async def write_buffer_to_blob(buffer, state: StateHelper, filename: str):
+  safe_filename = filename.replace(" ", "_")
   buffer.seek(0)
   client = state.storage
-  await client.upload_blob(data=buffer, name=filename, overwrite=True)
+  await client.upload_blob(data=buffer, name=safe_filename, overwrite=True)
